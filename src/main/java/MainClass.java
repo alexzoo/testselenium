e7d656ca425cac01dedb5256397d14bb25613036
Alexey Zubov <zubov.alexey@gmail.com>
2018-03-27 18:59:24 +0300

    добавил MainClass

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,2 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by itdev4 on 27.03.2018.
+ */
 public class MainClass {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.seleniumhq.org");
+
+    }
 }
