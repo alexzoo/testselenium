3683839c6d91d862d89a77f3f3c56d4dfc55f2bb
Alexey Zubov <zubov.alexey@gmail.com>
2018-03-28 19:01:10 +0300

    изучение методов findElement

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by itdev4 on 27.03.2018.
@@ -9,8 +13,21 @@ public class MainClass {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
-        driver.get("https://www.seleniumhq.org");
+        driver.get("https://en.wikipedia.org");
+
+        WebElement link = driver.findElement(By.linkText("Log in"));
+        WebElement link2 = driver.findElement(By.partialLinkText("Donate"));
+        WebElement searchField = driver.findElement(By.name("search"));
+        WebElement searchButton = driver.findElement(By.className("searchButton"));
+        WebElement li = driver.findElement(By.id("ca-viewsource"));
+        WebElement input = driver.findElement(By.tagName("input"));
+        WebElement logo = driver.findElement(By.xpath("//a[@class='mw-wiki-logo']"));
+
+
+        driver.quit();
 
     }
 }
