bd9245c666686220da582d6710ca085948b345b9
Alexey Zubov <alexzoo@Alexeys-MacBook-Pro.local>
2018-05-11 18:23:09 +0300

    actions

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,8 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by itdev4 on 27.03.2018.
@@ -16,25 +17,20 @@ public class MainClass {
 
         WebDriver driver = new ChromeDriver();
 
-        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.get("https://www.w3schools.com/html/html_tables.asp");
+        driver.get("http://ebay.com");
 
-        WebDriverWait wait = (new WebDriverWait(driver, 5));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h1[contains(text(), 'HTML')]")));
+        WebElement link = driver.findElement(By.xpath("//tr[@role='list']//a[text()='Электроника']"));
+
+        Actions actions = new Actions(driver);
+
+        actions.moveToElement(link).build().perform();
 
 
-        WebElement tableElement = driver.findElement(By.xpath("//table[@id=\"customers\"]"));
 
-        Table table = new Table(tableElement, driver);
-
-        System.out.println("Rows number is: " + table.getRows().size());
-        System.out.println(table.getValueFromCell(2,3));
-        System.out.println(table.getValueFromCell(4,"Company"));
-        System.out.println(table.getValueFromCell(2,"Contact"));
-
-        driver.quit();
+        //driver.quit();
 
     }
 
